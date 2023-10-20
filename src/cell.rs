cc649dbfa943775a6605933f72faad47428598ac
Joya, Edgar <edgar@praeparet.io>
2023-10-19 22:42:28 -0400

    Cleaning up relay cvode for execution

--- src/cell.rs
+++ src/cell.rs
@@ -14,13 +14,6 @@ use crate::run_rust::{create_rust_file_compile_and_run};
 // will also be delegation logic to determine what format the content of the note should look like
 // along with auto tagging in the Note struct using the tag method.
 
-// Pass the signed note inside the enum.
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(tag = "cell_type")]
-pub enum Cell {
-    Code(CodeCell),
-    Output(OutputCell),
-}
 
 //Factory that build cells from signed_notes.
 #[derive(Debug, Serialize, Deserialize)]
@@ -40,21 +33,41 @@ impl CellFactory {
     pub fn execute(self) {
         match self {
             CellFactory::Code(s) => {
+                println!(
+                    "Code Cell {}",
+                    to_string_pretty(&s).unwrap()
+                );
                 let codecell = Code::new(s);
                 tokio::spawn(async move {
-                     codecell.run().await;
+                    let signed_note: SignedNote = codecell.run();
+                    let url = var("URL").unwrap_or("ws://localhost:7001".to_string());
+                    if let Ok(relay) = NostrRelay::new(&url).await {
+                        relay.send_note(signed_note).await.expect("Failed to send note");
+                    }
                 });
             }
             CellFactory::Output(s) => {
-                let output_cell: OutputCell = serde_json::from_str(
-                    s.get_content()
-                ).unwrap();
                 println!(
-                    "Output Cell: {:?}",
-                    to_string_pretty(&output_cell).unwrap()
+                    "Output Cell: {}",
+                    to_string_pretty(&s).unwrap()
                 );
             }
         }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CodeContent {
+    source: Vec<String>,
+}
+
+impl CodeContent {
+    pub fn new(source: Vec<String>) -> Self {
+        CodeContent { source }
+    }
+    // Expect the source to be a a vector of strings.
+    pub fn get_source(self) -> Vec<String> {
+        self.source
     }
 }
 
@@ -63,14 +76,38 @@ pub struct Code {
     signed_note: SignedNote,
 }
 
+
 impl Code {
     pub fn new(s: SignedNote) -> Self {
         Code { signed_note: s }
     }
-    pub async fn run(self) {
+    pub fn create_output_note(self, stdout: String) -> SignedNote {
+        let file_path = retrieve_pem_file_path(
+            "private-diego-stash.pem".to_string()
+        ).unwrap();
+        let user_key_pair = LocalKeyManagerOpenssl::new(file_path);
+        let mut note = Note::new(
+            user_key_pair.get_public_key(),
+            301,
+            &stdout
+        );
+        note.tag_note("u", self.signed_note.get_pubkey());
+        note.tag_note("a", self.signed_note.get_id());
+        let signed_note = user_key_pair.sign_nostr_event(note);
+        signed_note
+    }
+    pub fn run(self) -> SignedNote {
         // we will take the &self.signed_note, and create an output cell. The output_cell
         // should have the stdout with kind 301 ready to be sent to the relay.
         // The structure of the new cell can be made by 
+        let code_content: CodeContent = serde_json::from_str(
+            self.signed_note.get_content()
+        ).unwrap();
+        let stdout = create_rust_file_compile_and_run(
+            code_content.get_source()
+        );
+        let signed_note = self.create_output_note(stdout.unwrap());
+        signed_note
     }
 }
 
@@ -85,121 +122,5 @@ impl Output {
     }
     pub fn hello_world(&self) {
         println!("Hello World!");
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct CodeCell {
-    source: Vec<String>,
-    language: String,
-}
-
-impl CodeCell {
-    pub fn new(source: Vec<String>, language: String) -> Self {
-        CodeCell { source, language }
-    }
-    pub fn create_cell_content(self) -> String {
-        // I want to be able to take the code and language and create a json string that will be used in the code not.
-        let json_string = json!({
-          "language": self.language,
-          "source": self.source,
-          "cell_type": "Code"
-        });
-        json_string.to_string()
-    }
-    pub fn create_code_note(self) -> SignedNote {
-        let file_path = retrieve_pem_file_path(
-            "private-diego-stash.pem".to_string()
-        ).unwrap();
-        let user_key_pair = LocalKeyManagerOpenssl::new(file_path);
-        let mut note = Note::new(
-            user_key_pair.get_public_key(),
-            300,
-            &self.create_cell_content(),
-        );
-        note.tag_note("l", "rust");
-        note.tag_note("i", "cellID");
-        let signed_note = user_key_pair.sign_nostr_event(note);
-        signed_note
-    }
-    pub fn get_source(self) -> Vec<String> {
-        self.source
-    }
-    // I would like to have a run function that will use the
-    // methods inside this implementation to run the the create_code_note
-    // function and send out the note to the relay.
-    pub async fn run(self) {
-        let signed_code_note = self.create_code_note();
-        println!(
-            "Signed Code Note: {}",
-            to_string_pretty(&signed_code_note).unwrap()
-        );
-        let url = var("URL").unwrap_or("ws://localhost:7001".to_string());
-        if let Ok(relay) = NostrRelay::new(&url).await {
-            relay.send_note(signed_code_note).await.expect("Failed to send note");
-        }
-    }
-    pub async fn create_output_request(self) {
-        println!("Create Output Request {}", to_string_pretty(&self).unwrap());
-        let signed_code_note = self.create_code_note();
-        let current_cell: CodeCell = serde_json::from_str(
-            &signed_code_note.get_content()
-        ).unwrap();
-        let stdout = create_rust_file_compile_and_run(
-            current_cell.get_source()
-        ).unwrap();
-        let output_cell = OutputCell::new(stdout);
-        output_cell.run().await;
-
-    }
-}
-
-// The OutputCell will have the structure of the stdout
-// this stdout will be the result of the code cell
-// The stdout will be a string
-// the new note created will have the tags activeCell and publickey
-// of the user that created the code cell
-#[derive(Serialize, Deserialize, Debug)]
-pub struct OutputCell {
-    stdout: String,
-}
-
-impl OutputCell {
-    pub fn new(stdout: String) -> Self {
-        OutputCell { stdout }
-    }
-    pub fn create_output_content(self) -> String {
-        let json_string = json!({
-          "stdout": self.stdout,
-          "cell_type": "Output"
-        });
-        json_string.to_string()
-    }
-    pub fn create_output_note(self) -> SignedNote {
-        let file_path = retrieve_pem_file_path(
-            "private-diego-stash.pem".to_string()
-        ).unwrap();
-        let user_key_pair = LocalKeyManagerOpenssl::new(file_path);
-        let mut note = Note::new(
-            user_key_pair.get_public_key(),
-            301,
-            &self.create_output_content(),
-        );
-        note.tag_note("l", "rust");
-        note.tag_note("i", "0:cellId");
-        note.tag_note("a", "activeCellID");
-        let signed_note = user_key_pair.sign_nostr_event(note);
-        signed_note
-    }
-    pub async fn run(self) {
-        let signed_output_note = self.create_output_note();
-        println!(
-            "Signed Output Note: {}",
-            to_string_pretty(&signed_output_note).unwrap()
-        );
-        let url = var("URL").unwrap_or("ws://localhost:7001".to_string());
-        if let Ok(ws_connection) = NostrRelay::new(&url).await {
-            let _ = ws_connection.send_note(signed_output_note).await;
-        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod utils;
 mod cell;
 mod local_key_manager_openssl;
 mod run_rust;
-use crate::cell::{Cell, CellFactory};
+use crate::cell::{CellFactory};
 use crate::utils::{
     unix_stamp_from_the_beginning_of_today,
     append_key_value_to_json_string
@@ -16,85 +16,29 @@ use tokio::time::{sleep, Duration};
 use crate::local_key_manager_openssl::LocalKeyManagerOpenssl;
 use crate::utils::retrieve_pem_file_path;
 
-// I would like to create a code cell that looks like
-// {
-  // "id": "1234567890abcdef1234567890abcdef12345678",
-  // "pubkey": "01234567890abcdef1234567890abcdef12345678",
-  // "created_at": 1661040000,
-  // "kind": 402,
-  // "tags": [
-    // ["i", "#1"],
-    // ["N", "pubkeyOfNotebook"]
-  // ],
-  // "content": "This is a signed Nostr note.",
-  // "sig": "01234567890abcdef1234567890abcdef1234567801234567890abcdef1234567890abcdef12345678"
-// }
+fn create_code_content() -> String {
+    let json_string = json!({
+        "source": ["println!(\"Hello World!\");"]
+    });
+    json_string.to_string()
+
+}
 async fn create_code_cells(){
     sleep(Duration::from_secs(5)).await;
     let notebook_pem_file_path = retrieve_pem_file_path("notebook.pem".to_string()).unwrap();
     let notebook_key_pair = LocalKeyManagerOpenssl::new(notebook_pem_file_path);
     let user_pem_file_path = retrieve_pem_file_path("private-diego-stash.pem".to_string()).unwrap();
     let user_key_pair = LocalKeyManagerOpenssl::new(user_pem_file_path);
-    let content_string: String = json!({
-        "source":"println!(\"Hello World\")"
-    }).to_string();
     let mut note = Note::new(
         user_key_pair.get_public_key(),
         300,
-        &content_string
+        &create_code_content()
     );
     note.tag_note("i", "#1");
     note.tag_note("N", &notebook_key_pair.get_public_key());
     let signed_note = user_key_pair.sign_nostr_event(note);
     let cell: CellFactory = CellFactory::identify_note_type(signed_note);
     cell.execute();
-}
-
-async fn listen_to_relay(){
-    let url = env::var("URL").unwrap_or("ws://localhost:7001".to_string());
-    if let Ok(relay) = NostrRelay::new(&url).await {
-    let filter: Value = json!({
-        "kinds":[300,301],
-        // unix timespamp since the beginning of today
-        "since": unix_stamp_from_the_beginning_of_today(),
-        "limit": 100,
-    });
-        relay.subscribe(filter).await.expect("Could not subscribe");
-        // let _ = ws_connection.send_note(signed_code_note).await;
-        while let Some(Ok(message)) = relay.read_from_relay().await {
-                match message {
-                    RelayEvents::EVENT(_event,_id, signed_note) => {
-                        println!(
-                            "Received event: {}",
-                            to_string_pretty(&signed_note).unwrap()
-                        );
-                        ////////////////////////////
-                        // 1) Retrieve a rust code note
-                        // 2) Send to create_output_request
-                        ////////////////////////////
-                        let current_cell: Cell = serde_json::from_str(
-                            signed_note.get_content()
-                        ).unwrap();
-                        let rust_file_output = match current_cell {
-                            Cell::Code(c) => c.create_output_request().await,
-                            Cell::Output(o) => println!(
-                                "Output Cell Matched: {}",
-                                to_string_pretty(&signed_note).unwrap()
-                            ),
-                            _ => panic!("Not a code cell"),
-                        };
-                        ////////////////////////////
-                    },
-                    RelayEvents::OK(_event, id, success, _msg) =>{
-                        println!("Received Ok: {} {} {}", id, success, _msg);
-                    },
-                    _ => {
-                        println!("Received something else");
-                    }
-                }
-            }
-        }
-
 }
 
 async fn create_from_signed_note() {
